dd284e83ebe608c6838408f8b7d542406b17a9bc
Lamar <github@lamargreene.com>
2016-01-30 19:34:05 -0600

    initial commit

---
+++ FlappyBird/GameScene.swift
@@ -0,0 +1,25 @@
+//
+//  GameScene.swift
+//  FlappyBird
+//
+//  Created by Lamar Greene on 1/30/16.
+//  Copyright (c) 2016 Lamar Greene. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMoveToView(view: SKView) {
+
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+       /* Called when a touch begins */
+
+        
+    }
+   
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
