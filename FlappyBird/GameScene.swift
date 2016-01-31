793aa59ea075af2bf31b38352e44a4183384a1eb
Lamar <github@lamargreene.com>
2016-01-30 19:55:57 -0600

    Added Bird to Screen
    Added 2 bird images, and created perpetual animation between them.
    added animation to scene

--- FlappyBird/GameScene.swift
+++ FlappyBird/GameScene.swift
@@ -9,13 +9,32 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    var bird = SKSpriteNode()
+    
     override func didMoveToView(view: SKView) {
+        
+        let birdTexture = SKTexture(imageNamed: "flappy1.png")
+        let birdTexture2 = SKTexture(imageNamed: "flappy2.png")
+        
+        let animation = SKAction.animateWithTextures([birdTexture, birdTexture2], timePerFrame: 0.1)
+        let makeBirdFlap = SKAction.repeatActionForever(animation)
+        
+        bird = SKSpriteNode(texture: birdTexture)
+        
+        bird.position = CGPoint(x: CGRectGetMidX(self.frame), y: CGRectGetMidY(self.frame))
+        
+        bird.runAction(makeBirdFlap)
+        
+        self.addChild(bird)
 
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
        /* Called when a touch begins */
 
+        
+        
         
     }
    
